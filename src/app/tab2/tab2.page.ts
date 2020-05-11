669ceaf354c62aa6b3cfafb7a7a84de4b4ecaf66
Vicente Garza Reyna <vgarzareyna@gmail.com>
2020-05-10 23:52:16 -0500

    Toots working for a given instance and token

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { GeneralService } from 'src/services/general.service';
+
 
 @Component({
   selector: 'app-tab2',
@@ -6,7 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  private instanceURL :string = "https://botsin.space";
+  private token :string = "hNAusBxqZEfPFKPYrjJ213Vou9GzZRBfnr1AWQPC9vw";
+  @Input() private text:string;
 
-  constructor() {}
+  constructor( private general:GeneralService ) {}
+
+  postToot(): void {
+    this.general.postToot(this.instanceURL, this.token, this.text).subscribe(
+      (data)=>{
+        alert("Posted Succesfully");
+      },
+      (error)=>{
+        alert("Something Happened...")
+      }
+    )
+  }
 
 }
